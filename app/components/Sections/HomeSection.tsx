9b70c6e9c6a62158b92cdc9ddc5f30193b12082c
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-07-24 07:58:26 +0700

    fix: fixing home section

--- app/components/Sections/HomeSection.tsx
+++ app/components/Sections/HomeSection.tsx
@@ -20,7 +20,7 @@ function HomeSection(): React.JSX.Element {
                 <div id="content" className="opacity-0 transform translate-y-10 transition-all duration-700 flex flex-col items-center lg:items-start lg:w-1/2 text-center lg:text-left p-4">
                     <p className="text-gray-700 dark:text-gray-400 text-lg">Haii..👋</p>
                     <h1 className="text-4xl font-bold mb-4">Saya <span className="text-4xl text-teal-500">{nama}</span></h1>
-                    <p className="text-lg font-medium mb-2 text-gray-700 dark:text-gray-400">Sebagai</p>
+                    <p className="text-lg font-medium mb-2 text-gray-700 dark:text-gray-400">Seorang</p>
                     <div className="text-3xl font-semibold mb-4 italic text-teal-500">
                         {role}  🧑🏻‍💻
                     </div>
