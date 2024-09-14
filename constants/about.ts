0989f08e6916dbe8e00998d82cd2516792c8c298
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-09-14 21:47:38 +0700

    refactor: edit portfolio and about section

--- constants/about.ts
+++ constants/about.ts
@@ -6,7 +6,7 @@ export const about: About[] = [
         negara: 'Malang City',
         role: 'Undergraduate Informatics Engineering Student',
         CV: '/assets/CV_Ahmad_Ammar.pdf',
-        deskripsi: 'Hi everyone! I am Ahmad Ammar Musyaffa, an informatics engineering student who is interested in technology and web development. I am currently studying at the State University of Malang and I always try to learn new things about technologies.',
+        deskripsi: 'Hi everyone! I am Ahmad Ammar Musyaffa, an informatics engineering student interested in technology and web development. I am currently studying at the State University of Malang. I am also a freelance web developer who has experience in developing web-based applications using various technologies such as React, Next.js, and Laravel. I am also a person who is always eager to learn new things and always tries to improve myself in the field of technology.',
         foto: '/assets/ammar-blur.jpg'
     }
 ]
--- constants/portfolio.ts
+++ constants/portfolio.ts
@@ -36,13 +36,6 @@ export const fullPortfolio: Portfolio[] = [
         source: "https://github.com/ahmadammarm/eclipse-glow"
     },
     {
-        nama: "Bu Aini's Catering Information System",
-        gambar: "/assets/portfolios/sicaba.png",
-        deskripsi: "Web-based information system for Bu Aini's catering management located in Malang City.",
-        teknologi: ["/assets/skills/php.png", "/assets/skills/laravel.png", "/assets/skills/bootstrap.png"],
-        source: "https://github.com/HarunSatr/SICABA"
-    },
-    {
         nama: "Webcademy Learning Platform",
         gambar: "/assets/portfolios/webcademy.png",
         deskripsi: "An online learning platform that provides various video-based courses on web programming.",
@@ -51,18 +44,11 @@ export const fullPortfolio: Portfolio[] = [
         source: "https://github.com/ahmadammarm/webcademy"   
     },
     {
-        nama: "Bakso Prima Cak Pen Landing Page",
-        gambar: "/assets/portfolios/bakso.png",
-        deskripsi: "Landing page for Bakso Prima Cak Pen, a meatball restaurant located in Malang City.",
-        teknologi: ["/assets/skills/html.png", "/assets/skills/css.png", "/assets/skills/js.png", "/assets/skills/bootstrap.png"],
-        link: "https://bakso-prima-cak-pen.vercel.app/",
-        source: "https://github.com/ahmadammarm/bakso-prima"
-    },
-    {
         nama: "Freelance Notes System with Invoice",
         gambar: "/assets/portfolios/freelance-notes.png",
         deskripsi: "Web-based system for recording freelance work and generating invoices.",
         teknologi: ["/assets/skills/php.png", "/assets/skills/laravel.png"],
+        source: "https://github.com/ahmadammarm/freelance-notes"
     },
     {
         nama: "Daily Prayer and Dhikr Website",
@@ -80,11 +66,10 @@ export const fullPortfolio: Portfolio[] = [
         source: "https://github.com/ahmadammarm/game-kemerdekaan"
     },
     {
-        nama: "Shopee Affiliate Website",
-        gambar: "/assets/portfolios/affiliate.png",
-        deskripsi: "Affiliate website that provides various products from Shopee.",
-        teknologi: ["/assets/skills/html.png", "/assets/skills/css.png", "/assets/skills/js.png"],
-        link: "https://affiliate-website-one.vercel.app/",
-        source: "https://github.com/ahmadammarm/affiliate-website"
+        nama: "Aishino Food Landing Page",
+        gambar: "/assets/portfolios/aishino.png",
+        deskripsi: "Landing page for Aishino Food, a food delivery service located in Malang City.",
+        teknologi: ["/assets/skills/react.png", "/assets/skills/tailwind.png"],
+        link: "https://aishino-food.vercel.app/",
     },
 ]
