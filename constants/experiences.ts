2bdaade3f7023dee5eb4ae087c32e7c93c729491
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-12-22 12:45:05 +0700

    adding mysql

--- constants/experiences.ts
+++ constants/experiences.ts
@@ -4,7 +4,7 @@ export const experiences: Experiences[] = [
     {
         nama: "Independent Freelancer",
         jabatan: "Full Stack Web Developer",
-        logo: ["/assets/skills/laravel.png", "/assets/skills/react.png", "/assets/skills/nextjs.png", "/assets/skills/tailwind.png", "/assets/skills/bootstrap.png"],
+        logo: ["/assets/skills/laravel.png", "/assets/skills/react.png", "/assets/skills/nextjs.png", "/assets/skills/tailwind.png", "/assets/skills/bootstrap.png", "/assets/skills/mysql.png"],
         kontrak: "Freelance",
         deskripsi: "Responsible for providing services in website development according to clients' needs and requirements using frameworks such as Laravel and NextJs.",
         tahun: "August 2023 - Present"
@@ -12,7 +12,7 @@ export const experiences: Experiences[] = [
     {
         nama: "Exoria Serana Digital",
         jabatan: "Full Stack Web Developer",
-        logo: ["/assets/skills/laravel.png" , "/assets/skills/bootstrap.png"],
+        logo: ["/assets/skills/laravel.png" , "/assets/skills/bootstrap.png", "/assets/skills/mysql.png"],
         kontrak: "Freelance",
         deskripsi: "Develop the company profile website based on a content management system using Laravel framework ",
         tahun: "December 2023 - July 2024"
@@ -20,7 +20,7 @@ export const experiences: Experiences[] = [
     {
         nama: "PUI-PT DLI State University of Malang",
         jabatan: "Full Stack Web Developer",
-        logo: ["/assets/skills/laravel.png", "/assets/skills/tailwind.png"],
+        logo: ["/assets/skills/laravel.png", "/assets/skills/tailwind.png", "/assets/skills/mysql.png"],
         kontrak: "Freelance",
         deskripsi: "Using Laravel framework to build education web based applications according to clients need. Maintained and updated application based on feedback and runtime requirement.",
         tahun: "June 2024 - Present"
@@ -28,7 +28,7 @@ export const experiences: Experiences[] = [
     {
         nama: "Pastijadi Digital Solutions",
         jabatan: "Full Stack Web Developer",
-        logo: ["/assets/skills/laravel.png", "/assets/skills/bootstrap.png"],
+        logo: ["/assets/skills/laravel.png", "/assets/skills/bootstrap.png", "/assets/skills/mysql.png"],
         kontrak: "Freelance",
         deskripsi: "Responsible for developing and customizing websites according to detailed client requirements using framework such as Laravel.",
         tahun: "August 2024 - Present"
--- constants/skill.ts
+++ constants/skill.ts
@@ -2,75 +2,80 @@ import { Skill } from "@/types/skill";
 
 export const techStack: Skill[] = [
     {
-        gambar: "assets/skills/html.png",
+        gambar: "/assets/skills/html.png",
         nama: "HTML",
         tingkat: "Advance",
     },
     {
-        gambar: "assets/skills/css.png",
+        gambar: "/assets/skills/css.png",
         nama: "CSS",
         tingkat: "Intermediate",
     },
     {
-        gambar: "assets/skills/js.png",
+        gambar: "/assets/skills/js.png",
         nama: "Javascript",
         tingkat: "Intermediate",
     },
     {
-        gambar: "assets/skills/ts.png",
+        gambar: "/assets/skills/ts.png",
         nama: "Typescript",
         tingkat: "Basic",
     },
     {
-        gambar: "assets/skills/php.png",
+        gambar: "/assets/skills/php.png",
         nama: "PHP",
         tingkat: "Basic",
     },
     {
-        gambar: "assets/skills/bootstrap.png",
+        gambar: "/assets/skills/bootstrap.png",
         nama: "Bootstrap",
         tingkat: "Intermediate",
     },
     {
-        gambar: "assets/skills/laravel.png",
+        gambar: "/assets/skills/laravel.png",
         nama: "Laravel",
         tingkat: "Basic",
     },
     {
-        gambar: "assets/skills/react.png",
+        gambar: "/assets/skills/react.png",
         nama: "React",
         tingkat: "Basic",
     },
     {
-        gambar: "assets/skills/nextjs.png",
+        gambar: "/assets/skills/nextjs.png",
         nama: "NextJs",
         tingkat: "Basic",
     },
     {
-        gambar: "assets/skills/tailwind.png",
+        gambar: "/assets/skills/tailwind.png",
         nama: "Tailwind CSS",
         tingkat: "Intermediate",
+    },
+    {
+        gambar: "/assets/skills/mysql.png",
+        nama: "MySQL",
+        tingkat: "Basic",
     },
 ];
 
 export const tools: Skill[] = [
     {
-        gambar: "assets/skills/figma.png",
+        gambar: "/assets/skills/figma.png",
         nama: "Figma",
         tingkat: "Basic",
     },
     {
-        gambar: "assets/skills/git.png",
+        gambar: "/assets/skills/git.png",
         nama: "Git",
         tingkat: "Intermediate",
     },
     {
-        gambar: "assets/skills/github.png",
+        gambar: "/assets/skills/github.png",
         nama: "Github",
         tingkat: "Intermediate",
     },
     {
-        gambar: 'assets/skills/vscode.png',
+        gambar: '/assets/skills/vscode.png',
         nama: 'Visual Studio Code',
         tingkat: 'Intermediate',
     }
