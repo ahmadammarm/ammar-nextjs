4615b508df730bef0ee4dec5ceca0fe5dfd61cd2
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-12-23 17:02:25 +0700

    editing photo in about page

--- constants/about.ts
+++ constants/about.ts
@@ -7,6 +7,6 @@ export const about: About[] = [
         role: 'Undergraduate Informatics Engineering Student',
         CV: '/assets/CV_Ahmad_Ammar.pdf',
         deskripsi: 'Hi everyone! My name is Ahmad Ammar Musyaffa, and Im an informatics engineering student who interested in technology and web development. I am currently studying at the State University of Malang. I am also a freelance web developer who has experience in developing web-based applications using various technologies such as React, Next.js, and Laravel. I am also a person who is always eager to learn new things and always tries to improve myself in the field of technology.',
-        foto: '/assets/ammar-baru.jpg'
+        foto: '/assets/ammar-jaket-copy.jpg'
     }
 ]
