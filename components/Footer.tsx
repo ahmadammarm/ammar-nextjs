09f31b309fb661395ed73b90656cd3ac0b26cb3f
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-04-14 15:38:14 +0700

    refactor: about and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer(): React.ReactElement {
     return (
-        <div className="bg-teal-500 text-white py-6">
+        <div className="bg-teal-500 text-white py-6 px-4">
             <div className="text-center text-sm lg:text-base">
                 <p className="dark:text-slate-900">&copy; 2024 Created by Ahmad Ammar Musyaffa. All Rights Reserved.</p>
             </div>
--- constants/about.ts
+++ constants/about.ts
@@ -6,7 +6,7 @@ export const about: About[] = [
         negara: 'Malang City',
         role: 'Undergraduate Informatics Engineering Student',
         CV: '/assets/CV_Ahmad_Ammar.pdf',
-        deskripsi: 'Hi everyone! My name is Ahmad Ammar Musyaffa, and Im an informatics engineering student who interested in technology and web development. I am currently studying at the State University of Malang. I am also a freelance web developer who has experience in developing web-based applications using various technologies such as React, Next.js, and Laravel. I am also a person who is always eager to learn new things and always tries to improve myself in the field of technology.',
+        deskripsi: "My name is Ahmad Ammar Musyaffa and I'm an informatics engineering student at State University of Malang. I have a strong interest in software development, especially in back end web development. To affirm my interest and gained my skills in real projects, I also work as a freelancer to develop web-based applications according to client's needs. I'm always eager to learn new things and always try to improve myself in the field of technologies. I'm excited to connect with others in order to explore new opportunities and experiences.",
         foto: '/assets/ammar-jaket-copy.jpg'
     }
 ]
